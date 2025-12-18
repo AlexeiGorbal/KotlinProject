7e2aa488a8c212c1d5583cc6e32d3c849f5829b3
lemon <lemon.juice9341@gmail.com>
2025-12-18 16:28:04 +0300

    added LocationApi for get locations

---
+++ composeApp/src/androidMain/kotlin/org/example/project/search/LocationApi.kt
@@ -0,0 +1,12 @@
+package org.example.project.search
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface LocationApi {
+    @GET("search.json?")
+    suspend fun getLocationsByInput(@Query("q") input: String): List<LocationInfoEntity>
+
+    @GET("search.json?")
+    suspend fun getLocationByCoordinates(@Query("q") latLng: String): List<LocationInfoEntity>
+}
